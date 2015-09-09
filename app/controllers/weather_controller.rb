b0ae29155f7a955cad5f9800ed754877cb9955d3
colinjlacy <colin@webcake.co>
2015-09-09 16:17:23 -0500

    [api-ctrl] builds out the API call, adds bootstrap styling

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,7 +3,17 @@ class WeatherController < ApplicationController
   end
 
   def display
-	  response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_key']}/geolookup/conditions/q/LA/New_Orleans.json");
-	  @res = response.to_s
+	  response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_key']}/geolookup/conditions/q/LA/New_Orleans.xml")
+	  @res = {
+		  city: response['response']['location']['city'],
+		  state: response['response']['location']['state'],
+		  country: response['response']['location']['country'],
+		  updated: response['response']['current_observation']['observation_time'],
+		  weather: response['response']['current_observation']['weather'],
+		  weather_icon: response['response']['current_observation']['icon_url'],
+		  temp: response['response']['current_observation']['temp_f'],
+		  wind: response['response']['current_observation']['wind_string'],
+		  more_link: response['response']['current_observation']['forecast_url']
+	  }
   end
 end
