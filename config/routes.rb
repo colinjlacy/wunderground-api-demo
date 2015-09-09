2a6776afe9ddd5d84017d79d75756b179046c445
colinjlacy <colin@webcake.co>
2015-09-09 05:23:09 -0500

    [api-ctrl] builds controller, adds bootstrap to asset pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'weather/form'
+
+  get 'weather/display'
+
+	root 'weather#display'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
